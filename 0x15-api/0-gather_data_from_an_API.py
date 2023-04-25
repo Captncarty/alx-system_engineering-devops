bc4743174b073b6e083f43bbcb001430555a0fc1
kelvin <edithmukoro3@gmail.com>
2023-04-25 00:50:30 +0000

    all fix.... next task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(user_id)).json()
     todo = requests.get(url + "todos", params={"userId": user_id}).json()
     fix_in = [t.get("title") for t in todo if t.get("completed") is True]
-    print("Employee {} is done with task({}/{}):".format(
+    print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(fix_in), len(todo)))
     [print("\t {}".format(c)) for c in fix_in]
